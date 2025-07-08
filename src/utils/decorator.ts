bee3022bc87434a839197f31ab910365b3f3966b
Daniel Oaks <daniel@danieloaks.net>
2025-07-08 23:48:23 +1000

    Make trace output verbose logs

--- src/utils/decorator.ts
+++ src/utils/decorator.ts
@@ -1,9 +1,11 @@
+import Logger from "../logger";
+
 export function trace(eventName: string): MethodDecorator {
     return (target, propertyKey: string | symbol, descriptor: PropertyDescriptor) => {
         const originalMethod = descriptor.value;
 
         descriptor.value = function(...args: any[]) {
-            console.log("restclient:", eventName);
+            Logger.verbose(eventName)
             return originalMethod.apply(this, args);
         };
 
